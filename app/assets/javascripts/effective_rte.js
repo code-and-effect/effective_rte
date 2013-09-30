db3dcfebba1c3abd5a7ce0da5c29c520ec5a5035
Matt Riemer <matthew@agilestyle.com>
2013-09-30 12:42:02 -0600

    Remove vendor/ and use asset pipeline

--- app/assets/javascripts/effective_rte.js
+++ app/assets/javascripts/effective_rte.js
@@ -2,4 +2,3 @@
 //= require wymeditor/plugins/insert_assets/jquery.wymeditor.insert_assets
 //= require wymeditor/plugins/html_cleanup/jquery.wymeditor.html_cleanup
 //= require wymeditor/plugins/embed/jquery.wymeditor.embed
-//= require beautify-html
