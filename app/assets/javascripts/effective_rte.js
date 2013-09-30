7f75e615bad676b6eaa02656a9d3b727efe9bd20
Matt Riemer <matthew@agilestyle.com>
2013-09-30 12:00:08 -0600

    Include embed plugin

--- app/assets/javascripts/effective_rte.js
+++ app/assets/javascripts/effective_rte.js
@@ -1,4 +1,5 @@
 //= require wymeditor/jquery.wymeditor.js
 //= require wymeditor/plugins/insert_assets/jquery.wymeditor.insert_assets
 //= require wymeditor/plugins/html_cleanup/jquery.wymeditor.html_cleanup
+//= require wymeditor/plugins/embed/jquery.wymeditor.embed
 //= require beautify-html
